82c8d4ba58f55fd086206a208a5bbc2b3b3ecbb2
Filipp16 <gaev.f@bk.ru>
2024-05-23 23:45:42 +0500

    покдлючена база, но не полностью

--- app/streamlit.py
+++ app/streamlit.py
@@ -1,9 +1,25 @@
 import streamlit as st
 import sqlite3
 
+conn = sqlite3.connect('C:\\Users\\gaevf\\Downloads\\knowledge_base.db')
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM articles")
+rows = cursor.fetchall()
+
+files = {}
+for row in rows:
+    category = row[0]  # Первая колонка - категория
+    subcategory = row[1]  # Вторая колонка - подкатегория
+    subcategory_data = {row[1]: row[2] for row in rows if row[0] == category and row[1] == subcategory}
+
+    if category not in files:
+        files[category] = {}
+
+    files[category][subcategory] = subcategory_data
+
 left1_column, right2_column = st.columns([1, 4])
 inside_left, inside_right = right2_column.columns([4, 1])
-query = inside_left.text_input('', label_visibility='collapsed')
 # inside_right.markdown("<style>div.stButton > button:first-child { margin-top: 100px; }</style>", unsafe_allow_html=True)
 st.markdown(
     """
@@ -15,38 +31,61 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
-# Добавление вкладок
-# tabs = ["Главная", "Краткое описание", "Доп. Информация"]
-# selected_tab = st.sidebar.radio("Выберите вкладку", tabs)
+results = ''
 
 
-# Функция для обработки запроса и вывода данных
+def base():
+    components = {}
+    global results
+    print(files.values())
+    for categories in files.values():
+        for test in list(categories.keys()):
+            components[test] = st.expander(test)
+
+            for subcategory in categories.values():
+                print('xyuuuuuuuuuuuu', subcategory)
+                for title in list(subcategory.keys()):
+                    components[test].title(title)
+                    components[test].write(subcategory[title])
+                print(components.keys())
+
+    # print('aboba', str(list(theme.values())[:]))
+    # results += str(list(theme.values())[:])
+
+
 def process_query(query):
-    # Здесь можно добавить логику обработки запроса и получения данных
-    # Например, можно использовать pandas для работы с данными
-
-    # Пример вывода данных
+    global results
     right2_column.write(f'Вы ввели запрос: {query}')
-    st.write('Здесь могут быть данные, соответствующие вашему запросу')
+    print(files.values())
+    for subcategory in files.values():
+        for theme in subcategory.values():
+            if query in theme:
+                results = list(theme)[0]
 
 
-# Обработка запроса при нажатии кнопки
-if inside_right.button('🔍', key="my-button"):
+main_page = st.container()
+
+
+def create_list(data, level=0):
+    global results
+    for key, value in data.items():
+        with st.sidebar:
+            if isinstance(value, dict):
+                if st.checkbox('➖' * level + str(key)):
+                    create_list(value, level + 1)
+            else:
+                if st.checkbox('➖' * level + str(key)):
+                    with main_page:
+                        results = value
+
+
+query = st.sidebar.text_input('', label_visibility='collapsed')
+if st.sidebar.button('🔍', key="my-button"):
     process_query(query)
 
-# Определение структуры иерархии страниц и категорий
-pages = {
-    "Категория 1": ["Страница 1", "Страница 2"],
-    "Категория 2": ["Страница 3", "Страница 4", "Страница 5"],
-    "Категория 3": ["Страница 6"]
-}
+create_list(files)
 
-# Отображение названий страниц в боковой панели
-for category, subpages in pages.items():
-    st.sidebar.subheader(category)
-    for subpage in subpages:
-        if st.sidebar.button(subpage):
-            st.write(f"Отображается страница: {subpage}")
+st.write(results)
+
 # путь к базе
 # database_path = "C:\\Users\\gaevf\\PycharmProjects\\ProjectStreamlit\\wikipedia_articles.db"
