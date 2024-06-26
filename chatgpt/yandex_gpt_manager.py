b0c61b70545b8606fdeed639f3355ead68618803
Кирилл <41121179+kukhvatov@users.noreply.github.com>
2024-06-26 14:02:32 +0500

    Add files via upload

---
+++ chatgpt/yandex_gpt_manager.py
@@ -0,0 +1,30 @@
+import requests
+url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Api-Key AQVN2pHf4BRLAXpXFz9Saapxdm1cjanw-8iZK6-s" # Этот api  убрать
+}
+def fetch_yandex_gpt_response(question = None, answer = None):
+    prompt = {
+        "modelUri": "gpt://b1gks3qh14qma0kud95t/yandexgpt/latest",  # b1gks3qh14qma0kud95t Этот тоже убрать надо
+        "completionOptions": {
+            "stream": False,
+            "temperature": 0.6,
+            "maxTokens": "10000"
+        },
+        "messages": [
+            {
+                "role": "system",
+                "text": f"Дай ответ на вопрос {question} согласно контексту из базы знаний. Не придумывай лишней информации. Использую только то, что есть в базе знаний. Ответ дай максимально краткий."
+            },
+            {
+                "role": "user",
+                "text": answer
+            },
+        ]
+    }
+    response = requests.post(url, headers=headers, json=prompt)
+    answer = response.json()
+    answer = answer['result']['alternatives'][0]['message']['text']
+
+    return (answer)
